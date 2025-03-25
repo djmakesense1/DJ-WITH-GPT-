28cc989c41e6c90077609e029df15a0edac777aa
djmakesense1 <trzxdjx@naver.com>
2025-03-25 13:05:04 +0900

    Delete index.tsx

--- index.tsx
+++
@@ -1,9 +0,0 @@
-import ShippingProfitCalculator from "../components/ShippingProfitCalculator";
-
-export default function Home() {
-  return (
-    <main style={{ minHeight: "100vh", padding: "2rem", backgroundColor: "#f3f4f6" }}>
-      <ShippingProfitCalculator />
-    </main>
-  );
-}
