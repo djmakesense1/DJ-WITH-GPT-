38572f4293b60df1931fd72525cd6df413bc7c61
djmakesense1 <trzxdjx@naver.com>
2025-03-25 12:16:29 +0900

    Add files via upload

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import ShippingProfitCalculator from "../components/ShippingProfitCalculator";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen p-6 bg-gray-100">
+      <ShippingProfitCalculator />
+    </main>
+  );
+}
