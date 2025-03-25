e0a0f0e1f63231b2d7edb6b204d7e7f2d800670b
djmakesense1 <trzxdjx@naver.com>
2025-03-25 12:49:12 +0900

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import ShippingProfitCalculator from "../components/ShippingProfitCalculator";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen p-6 bg-gray-100">
-      <ShippingProfitCalculator />
-    </main>
-  );
-}
